0688f9427754c4f089ab70f9b19eea037a708411
EccentricPotato <36072143+EccentricPotato@users.noreply.github.com>
2019-04-26 01:25:14 +0300

    Add files via upload

--- mover.py
+++ mover.py
@@ -5,7 +5,7 @@ from time import sleep
 from ev3dev2.motor import MediumMotor, MoveSteering, MoveTank, OUTPUT_A, OUTPUT_B, OUTPUT_C
 from time import sleep
 from ev3dev2.sensor.lego import GyroSensor
-
+"""
 
 def MoveBackward(steering = 0, speed = 40):
     steer_pair = MoveSteering(OUTPUT_A, OUTPUT_B, motor_class=LargeMotor)
@@ -35,4 +35,7 @@ def MoveLeft():
 MoveForward()
 MoveRight()
 MoveLeft()
-sleep(1)
+sleep(1)"""
+tank_pair = MoveTank(OUTPUT_A, OUTPUT_B)
+tank_pair.off()
+sleep(5)
