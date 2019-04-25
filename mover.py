3c8e89e5fbc23ab09e7ac453c2f9410d902e8759
EccentricPotato <36072143+EccentricPotato@users.noreply.github.com>
2019-04-26 02:06:50 +0300

    Add files via upload

--- mover.py
+++ mover.py
@@ -21,8 +21,10 @@ def MoveRight():
     tank_pair = MoveTank(OUTPUT_A, OUTPUT_B)
     tank_pair.on(left_speed=100, right_speed=-100)
 
-    while True:  # while gyro sensor is not turned to 90 deg
+    while True:
+        print("The angle is:"+gy.value()+"\n")# while gyro sensor is not turned to 90 deg
         if(gy.value() > 2000):
+            print(gy.value())
             break
         sleep(0.01)
     tank_pair.off()
