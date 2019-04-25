a8279cf5884076a7dbdc723317eff86ca6e6154c
EccentricPotato <36072143+EccentricPotato@users.noreply.github.com>
2019-04-26 00:44:10 +0300

    Add files via upload

--- mover.py
+++ mover.py
@@ -14,12 +14,12 @@ def MoveForward(steering = 0, speed = -40):
     steer_pair = MoveSteering(OUTPUT_A, OUTPUT_B, motor_class=LargeMotor)
     steer_pair.on_for_seconds(steering, speed, seconds=1)
 
-def MoveRight()
+def MoveRight():
     tank_pair = MoveTank(OUTPUT_A, OUTPUT_B)
     tank_pair.on_for_degrees(left_speed = 20, right_speed = 0, degrees = 90, brake=True, block=True)
 
 
-def MoveLeft()
+def MoveLeft():
     tank_pair = MoveTank(OUTPUT_A, OUTPUT_B)
     tank_pair.on_for_degrees(left_speed=0, right_speed=20, degrees=90, brake=True, block=True)
 
