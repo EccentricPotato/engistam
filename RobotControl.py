3ad8b8578ee4cdba52b345f4637bac7c0cc1fe87
EccentricPotato <36072143+EccentricPotato@users.noreply.github.com>
2019-04-26 02:55:32 +0300

    Add files via upload

--- RobotControl.py
+++ RobotControl.py
@@ -6,6 +6,9 @@ from time import sleep
 from ev3dev2.sensor.lego import GyroSensor
 
 class Controls:
+    def Controls(self):
+
+
     def MoveBackward(steering=0, speed=40):
         steer_pair = MoveSteering(OUTPUT_A, OUTPUT_B, motor_class=LargeMotor)
         steer_pair.on_for_seconds(steering, speed, seconds=1)
--- mover.py
+++ mover.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-import RobotControl.Controls
+control = Controls()
 
-MoveForward()
+control.MoveForward()
