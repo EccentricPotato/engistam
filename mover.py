aefc08b58e290cb76bd0b0d7047716acf18c20ae
EccentricPotato <36072143+EccentricPotato@users.noreply.github.com>
2019-04-26 00:04:30 +0300

    Add files via upload

--- mover.py
+++ mover.py
@@ -3,7 +3,7 @@ from ev3dev2.motor import LargeMotor
 from ev3dev2.motor import SpeedDPS, SpeedRPM, SpeedRPS, SpeedDPM
 from time import sleep
 
-lm = LargeMotor()
+lm = LargeMotor("out B")
 
 '''
 This will run the large motor at 50% of its
