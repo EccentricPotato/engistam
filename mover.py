a9eb0baf581352bf74bc7192659295f3c51b9ca8
EccentricPotato <36072143+EccentricPotato@users.noreply.github.com>
2019-04-26 00:06:02 +0300

    Add files via upload

--- mover.py
+++ mover.py
@@ -3,12 +3,14 @@ from ev3dev2.motor import LargeMotor
 from ev3dev2.motor import SpeedDPS, SpeedRPM, SpeedRPS, SpeedDPM
 from time import sleep
 
-lm = LargeMotor("out B")
+lm1 = LargeMotor("out B")
+lm2 = LargeMotor("out A")
 
 '''
 This will run the large motor at 50% of its
 rated maximum speed of 1050 deg/s.
 50% x 1050 = 525 deg/s
 '''
-lm.on_for_seconds(speed = 50, seconds=3)
+lm1.on_for_seconds(speed = 50, seconds=3)
+lm2.on_for_seconds(speed = 50, seconds=3)
 sleep(1)
