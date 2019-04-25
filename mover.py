680a5562ddae569daccb578ecb3e3f6f0b420c6c
EccentricPotato <36072143+EccentricPotato@users.noreply.github.com>
2019-04-26 01:13:50 +0300

    Add files via upload

--- mover.py
+++ mover.py
@@ -2,8 +2,9 @@
 from ev3dev2.motor import LargeMotor
 from ev3dev2.motor import SpeedDPS, SpeedRPM, SpeedRPS, SpeedDPM
 from time import sleep
-from ev3dev2.motor import MediumMotor, MoveSteering, MoveTank, OUTPUT_A, OUTPUT_B
+from ev3dev2.motor import MediumMotor, MoveSteering, MoveTank, OUTPUT_A, OUTPUT_B, OUTPUT_C
 from time import sleep
+from ev3dev.core.Sensor import GyroSensor
 
 
 def MoveBackward(steering = 0, speed = 40):
@@ -15,8 +16,15 @@ def MoveForward(steering = 0, speed = -40):
     steer_pair.on_for_seconds(steering, speed, seconds=1)
 
 def MoveRight():
+    gy = GyroSensor(address = ev3:inC)
+    gy.mode = 'GYRO-ANG'
     tank_pair = MoveTank(OUTPUT_A, OUTPUT_B)
-    tank_pair.on_for_degrees(left_speed = 100, right_speed = -100, degrees = 300, brake=True, block=True)
+    tank_pair.on(left_speed=100, right_speed=-100)
+
+    while not gy.value() == 90:  # while touch sensor is not pressed
+        sleep(0.01)
+    tank_pair.off()
+    sleep(5)
 
 
 def MoveLeft():
