8a2ae4016b5a64aae24c36f381cc502a1da7982e
EccentricPotato <36072143+EccentricPotato@users.noreply.github.com>
2019-04-26 04:59:08 +0300

    Add files via upload

--- RightLeftMovement.py
+++ RightLeftMovement.py
@@ -24,6 +24,6 @@ class LeftRight:
         gy.mode = 'GYRO-ANG'
         tank_pair = MoveTank(OUTPUT_A, OUTPUT_B)
         tank_pair.on(left_speed=-15, right_speed=15)
-        gy.wait_until_angle_changed_by(delta=75)
+        gy.wait_until_angle_changed_by(delta=70)
         tank_pair.off()
         sleep(10)
